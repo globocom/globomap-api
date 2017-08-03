3483a605bfc169961d9386bdf36e65493ef22329
Ederson Brilhante <ederson.brilhante@corp.globo.com>
2017-08-02 22:01:47 -0300

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -10,8 +10,11 @@ setup(
     author_email='ederson.brilhante@corp.globo.com',
     install_requires=[
         'Flask==0.12.2',
+        'flask-cors==3.0.3',
+        'json-spec==0.10.1',
         'python-arango==3.8.0',
     ],
     url='https://github.com/globocom/globomap-api',
     packages=['globomap_api'],
+    package_data={'globomap_api': ['models/*.py', 'v1/*.py', 'specs/*.json']},
 )
