edb94dea41d22a0be53e69ebd2e7ce42ee6c6a7b
ederson.brilhante <ederson.brilhante@corp.globo.com>
2018-02-27 17:25:46 -0300

    Fixed connect in redis sentinel

--- globomap_api/models/redis.py
+++ globomap_api/models/redis.py
@@ -9,15 +9,15 @@ class RedisClient(object):
 
     def get_redis_conn(self):
         if config.REDIS_SENTINEL_ENDPOINT_SIMPLE:
+            redis_sentinels = [(rs, config.REDIS_SENTINELS_PORT)
+                               for rs in config.REDIS_SENTINELS.split(',')]
+            redis_service = config.REDIS_SENTINEL_SERVICE_NAME
+            redis_password = config.REDIS_SENTINEL_PASSWORD
             try:
-                sentinel = Sentinel(
-                    config.REDIS_SENTINELS, socket_timeout=0.1)
-                master = sentinel.discover_master(
-                    config.REDIS_SENTINEL_SERVICE_NAME)
+                sentinel = Sentinel(redis_sentinels, socket_timeout=0.1)
+                master = sentinel.discover_master(redis_service)
                 connection = redis.StrictRedis(
-                    host=master[0],
-                    port=master[1],
-                    password=config.REDIS_SENTINEL_PASSWORD)
+                    host=master[0], port=master[1], password=redis_password)
             except Exception as e:
                 app.logger.error(
                     'Failed to connect to redis Sentinel: {}'.format(e))
