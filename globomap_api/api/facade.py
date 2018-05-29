1e6312b2b09fb932d09d9c37ad7498c62e4e5924
ederson.brilhante <ederson.brilhante@corp.globo.com>
2018-05-29 16:41:01 -0300

    Remove unsed functions

--- globomap_api/api/facade.py
+++ globomap_api/api/facade.py
@@ -26,49 +26,6 @@ from globomap_api.models.db import DB
 from globomap_api.models.document import Document
 
 
-def create_graph(data):
-    """Create graph in Database"""
-
-    spec = app.config['SPECS'].get('graphs')
-    util.json_validate(spec).validate(data)
-
-    constructor = Constructor()
-    name = data.get('name')
-    links = data.get('links')
-    constructor.factory(
-        kind='Graph', create=True, name=name, links=links)
-
-    return True
-
-
-def create_collection_document(data):
-    """Create collection in Database"""
-
-    spec = app.config['SPECS'].get('collections')
-    util.json_validate(spec).validate(data)
-
-    constructor = Constructor()
-    name = data.get('name')
-    constructor.factory(
-        kind='Collection', create=True, name=name)
-
-    return True
-
-
-def create_collection_edge(data):
-    """Create edge in Database"""
-
-    spec = app.config['SPECS'].get('collections')
-    util.json_validate(spec).validate(data)
-
-    constructor = Constructor()
-    name = data.get('name')
-    constructor.factory(
-        kind='Edges', create=True, name=name)
-
-    return True
-
-
 def list_graphs():
     """Return all graph from Database"""
 
@@ -89,58 +46,6 @@ def list_collections(kind):
     collections = util.filter_collections(collections, kind)
 
     return collections
-
-
-def create_document(name, data):
-    """Create document in Database"""
-
-    spec = app.config['SPECS'].get('documents')
-    util.json_validate(spec).validate(data)
-
-    constructor = Constructor()
-    inst_coll = constructor.factory(kind='Collection', name=name)
-
-    document = data
-    document = {
-        '_key': util.make_key(data),
-        'id': data['id'],
-        'name': data.get('name', ''),
-        'provider': data['provider'],
-        'timestamp': data['timestamp'],
-        'properties': data.get('properties'),
-        'properties_metadata': data.get('properties_metadata')
-    }
-
-    inst_doc = Document(inst_coll)
-    doc = inst_doc.create_document(document)
-
-    return doc
-
-
-def create_edge(name, data):
-    """Create document-edge in Database"""
-
-    spec = app.config['SPECS'].get('edges')
-    util.json_validate(spec).validate(data)
-
-    constructor = Constructor()
-    inst_edge = constructor.factory(kind='Edges', name=name)
-    edge = {
-        '_key': util.make_key(data),
-        '_from': data['from'],
-        '_to': data['to'],
-        'id': data['id'],
-        'name': data.get('name', ''),
-        'provider': data['provider'],
-        'timestamp': data['timestamp'],
-        'properties': data.get('properties'),
-        'properties_metadata': data.get('properties_metadata')
-    }
-
-    inst_doc = Document(inst_edge)
-    doc = inst_doc.create_document(edge)
-
-    return doc
 
 
 def get_document(name, key):
@@ -165,147 +70,6 @@ def get_edge(name, key):
     doc = inst_doc.get_document(key)
 
     return doc
-
-
-def update_edge(name, key, data):
-    """Update edge from Database"""
-
-    get_edge(name, key)
-
-    spec = app.config['SPECS'].get('edges')
-    util.json_validate(spec).validate(data)
-    edge = {
-        '_key': key,
-        '_from': data['from'],
-        '_to': data['to'],
-        'id': data['id'],
-        'name': data.get('name', ''),
-        'provider': data['provider'],
-        'timestamp': data['timestamp'],
-        'properties': data.get('properties'),
-        'properties_metadata': data.get('properties_metadata')
-    }
-
-    constructor = Constructor()
-    inst_edge = constructor.factory(kind='Edges', name=name)
-
-    inst_doc = Document(inst_edge)
-    doc = inst_doc.update_document(edge)
-
-    return doc
-
-
-def update_document(name, key, data):
-    """Update document from Database"""
-
-    get_document(name, key)
-
-    spec = app.config['SPECS'].get('documents')
-    util.json_validate(spec).validate(data)
-    document = {
-        '_key': key,
-        'id': data['id'],
-        'name': data.get('name', ''),
-        'provider': data['provider'],
-        'timestamp': data['timestamp'],
-        'properties': data.get('properties'),
-        'properties_metadata': data.get('properties_metadata')
-    }
-
-    constructor = Constructor()
-    inst_coll = constructor.factory(kind='Collection', name=name)
-
-    inst_doc = Document(inst_coll)
-    doc = inst_doc.update_document(document)
-
-    return doc
-
-
-def patch_edge(name, key, data):
-    """Partial update edge from Database"""
-
-    edge = get_edge(name, key)
-
-    spec = app.config['SPECS'].get('edges_partial')
-    util.json_validate(spec).validate(data)
-    for key in data:
-        if key == 'from':
-            edge['_from'] = data[key]
-        elif key == 'to':
-            edge['_to'] = data[key]
-        elif key == 'properties':
-            for idx in data[key]:
-                edge['properties'][idx] = data[key][idx]
-        elif key == 'properties_metadata':
-            for idx in data[key]:
-                edge['properties_metadata'][idx] = data[key][idx]
-        elif key == 'name':
-            if data[key]:
-                edge[key] = data[key]
-        else:
-            edge[key] = data[key]
-
-    constructor = Constructor()
-    inst_edge = constructor.factory(kind='Edges', name=name)
-
-    inst_doc = Document(inst_edge)
-    doc = inst_doc.update_document(edge)
-
-    return doc
-
-
-def patch_document(name, key, data):
-    """Partial update document from Database"""
-
-    document = get_document(name, key)
-
-    spec = app.config['SPECS'].get('documents_partial')
-    util.json_validate(spec).validate(data)
-
-    for key in data:
-        if key == 'properties':
-            for idx in data[key]:
-                document['properties'][idx] = data[key][idx]
-        elif key == 'properties_metadata':
-            for idx in data[key]:
-                document['properties_metadata'][idx] = data[key][idx]
-        elif key == 'name':
-            if data[key]:
-                document[key] = data[key]
-        else:
-            document[key] = data[key]
-
-    constructor = Constructor()
-    inst_coll = constructor.factory(kind='Collection', name=name)
-
-    inst_doc = Document(inst_coll)
-    doc = inst_doc.update_document(document)
-
-    return doc
-
-
-def delete_document(name, key):
-    """Get document from Database"""
-
-    constructor = Constructor()
-    inst_coll = constructor.factory(kind='Collection', name=name)
-
-    inst_doc = Document(inst_coll)
-    inst_doc.delete_document(key)
-
-    return True
-
-
-def delete_edge(name, key):
-    """Get edge from Database"""
-
-    constructor = Constructor()
-    inst_edge = constructor.factory(kind='Edges', name=name)
-
-    inst_doc = Document(inst_edge)
-    inst_doc.delete_document(key)
-
-    return True
 
 
 def search_traversal(**kwargs):
@@ -381,20 +145,6 @@ def search(name, data, page, per_page):
     }
 
     return res
-
-
-def clear_collection(name, data):
-    """Clear document in Collection"""
-
-    spec = app.config['SPECS'].get('search')
-    util.json_validate(spec).validate(data)
-
-    db_inst = DB()
-
-    db_inst.get_database()
-    db_inst.clear_collection(name, data)
-
-    return {}
 
 
 def search_collections(collections, data, page, per_page):
