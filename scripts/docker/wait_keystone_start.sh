1f86e744040a53576cbc578a3e4db8648d2b844b
ederson.brilhante <ederson.brilhante@corp.globo.com>
2018-08-28 17:18:05 -0300

    Change docker-compose to fix delay in creating keystone container API wasn't starting

---
+++ scripts/docker/wait_keystone_start.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Check keystone service
+INTERVAL=5
+MAX_ATTEMPTS=30
+CHECK_COMMAND="curl -sL -w "%{http_code}" http://globomap_keystone:5000 -o /dev/null"
+ATTEMPTS=1
+HTTP_RET_CODE=`${CHECK_COMMAND}`
+while [ "000" = "${HTTP_RET_CODE}" ] && [ ${ATTEMPTS} -le ${MAX_ATTEMPTS} ]; do
+    echo "Error connecting to keystone. Attempt ${ATTEMPTS}. Retrying in ${INTERVAL} seconds ..."
+    sleep ${INTERVAL}
+    ATTEMPTS=$((ATTEMPTS+1))
+    HTTP_RET_CODE=`${CHECK_COMMAND}`
+done
+
+if [ "000" = "${HTTP_RET_CODE}" ]; then
+        echo "Error connecting to keystone. Aborting ..."
+        exit 2
+fi
+
+
+./scripts/docker/meta_collections.sh
+PYTHONPATH=`pwd`:$PYTHONPATH python3.6 globomap_api/run.py
